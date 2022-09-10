b6e553acf213ae734b55b05e5d7cccd36909574d
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-09-10 16:06:31 -0300

    feat: ensure mongodb recconects if client is down

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   roots: [
     '<rootDir>/src'
   ],
