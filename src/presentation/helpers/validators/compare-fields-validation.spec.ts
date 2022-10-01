de5b910adef5b74dc5db351eb55312dd378cb763
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-10-01 11:14:11 -0300

    test: ensure CompareFieldsValidation does not return if validation succeeds

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -13,4 +13,13 @@ describe('CompareFieldsValidation', () => {
     })
     expect(error).toEqual(new InvalidParamError('fieldToCompare'))
   })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_value',
+      fieldToCompare: 'any_value'
+    })
+    expect(error).toBeUndefined()
+  })
 })
