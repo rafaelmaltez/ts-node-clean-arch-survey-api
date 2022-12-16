a4b7e4ca4696dd73da547b7b85fa9c2945394985
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-12-16 19:53:42 -0300

    refactor: simplify constructors

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -3,13 +3,10 @@ import { badRequest, serverError, unauthorized, ok } from '../../helpers/http/ht
 import { Validation } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
-  private readonly authentication: Authentication
-  private readonly validation: Validation
-
-  constructor (authentication: Authentication, validation: Validation) {
-    this.authentication = authentication
-    this.validation = validation
-  }
+  constructor (
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+  ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
