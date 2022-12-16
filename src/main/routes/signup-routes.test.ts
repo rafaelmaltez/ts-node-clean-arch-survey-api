acafaf9c38d17ea828739cdfc9a08025ceb705ac
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-12-16 16:55:13 -0300

    fix: fix bcrypt adapter import at main

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -16,7 +16,7 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({})
   })
 
-  test('Should return an account on succes', async () => {
+  test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
       .send({
