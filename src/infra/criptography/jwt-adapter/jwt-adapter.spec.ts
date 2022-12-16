4aacac3789c0b33ea81714f39c4eec22bfe625bb
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-12-16 18:31:43 -0300

    test: ensure JwtAdapter returns an access token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -8,4 +8,11 @@ describe('Jwt Adapter', () => {
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => 'any_token')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
+  })
 })
