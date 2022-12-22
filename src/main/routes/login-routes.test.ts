6da571ca63085d30abc54703a0468d816f37d818
Rafael Maltez <rafaelmaltez89@gmail.com>
2022-12-22 15:01:18 -0300

    test: ensure login route returns 401 if invalid credentials are provided

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 if invalid credentials are provided', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'rafaelmaltez89@gmail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
   })
 })
