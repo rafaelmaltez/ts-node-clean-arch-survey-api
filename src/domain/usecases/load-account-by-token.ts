33957260566e85f9a7e7859de80d1497e724daa9
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-01-03 12:30:34 -0300

    feat: ensure AuthMiddleware returns 200 if LoadAccountByToken returns an account

--- src/domain/usecases/load-account-by-token.ts
+++ src/domain/usecases/load-account-by-token.ts
@@ -1,5 +1,5 @@
 import { AccountModel } from '../models/account'
 
 export interface LoadAccountByToken {
-  load: (accessToken: string) => Promise<AccountModel> | null
+  load: (accessToken: string) => Promise<AccountModel | null>
 }
