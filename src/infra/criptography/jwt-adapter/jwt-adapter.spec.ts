3bb740c3de39a3c35044d57c1671ff3adade2eb8
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-01-25 13:48:37 -0300

    test: ensure JwtAdapter returns a value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -45,5 +45,12 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a a value on verify success', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify')
+      const value = await sut.decrypt('any_token')
+      expect(value).toBe('any_value')
+    })
   })
 })
