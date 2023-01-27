7bfc94ef72f429c8cd457c3a083866d2d34986ac
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-01-27 09:43:01 -0300

    refactor: improve main layer folder structure

--- src/main/factories/controllers/survey/add-survey-factory.ts
+++ src/main/factories/controllers/survey/add-survey-factory.ts
@@ -1,7 +1,7 @@
 import { Controller } from '../../../../presentation/protocols/controller'
 import { AddSurveyController } from '../../../../presentation/controllers/survey/add-survey/add-survey-controller'
 import { makeSurveyValidation } from './survey-validation-factory'
-import { makeDbAddSurvey } from '../../usecases/add-survey/db-add-survey-factory'
+import { makeDbAddSurvey } from '../../usecases/survey/add-survey/db-add-survey-factory'
 import { makeLogControllerDecorator } from '../../decorators/log/log-controller-decorator-factory'
 
 export const makeSurveyController = (): Controller => {
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
-import { adaptRoute } from '../adapters/express/express-route-adapter'
-import { makeSignUpController } from '../factories/controllers/signup/signup-factory'
-import { makeLoginController } from '../factories/controllers/login/login-factory'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { makeSignUpController } from '../factories/controllers/login/signup/signup-factory'
+import { makeLoginController } from '../factories/controllers/login/login/login-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
